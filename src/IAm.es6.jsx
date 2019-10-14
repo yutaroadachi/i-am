c9c6a333d06c8d81023ef6d14e2fc3bc001931f8
yutaroadachi <ytr_adc72@yahoo.co.jp>
2019-10-14 17:23:03 +0900

    feat: Materialize導入&ヘッダー作成

---
+++ src/IAm.es6.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import IAmHeader from "./IAmHeader.es6";
+
+const iAm = () => {
+  return (
+    <React.Fragment>
+      <IAmHeader />
+      <p>Hello World!!</p>
+    </React.Fragment>
+  );
+};
+
+export default iAm;
---
+++ src/IAmHeader.es6.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const IAmHeader = () => {
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <a href="/" className="brand-logo center">
+          I am.
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default IAmHeader;
